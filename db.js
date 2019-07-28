b4b1fa4a2c75e3567de186eca0752469e167c8aa
montu <montu@gmail.com>
2019-07-28 21:41:16 +0530

    "1st try"

---
+++ db.js
@@ -0,0 +1,28 @@
+const Sequelize=require('sequelize')
+const {app}=require('./server~')
+
+const sequelize=new Sequelize({
+    dialect:'sqlite',
+    storage:'info.db'
+})
+
+const db=sequelize.define('info',{
+    idNO:{
+        type:Sequelize.INTEGER
+    },
+    name:{
+        type:Sequelize.STRING
+    },
+    info:{
+        type:Sequelize.STRING
+    },
+    form:{
+        type:Sequelize.STRING
+    }
+})
+
+module.exports={sequelize,db}
+
+db.sync().then(()=>{
+    app.listen(3000)
+})
